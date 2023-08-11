427d2508b61d293a550f5710d443940654a56c69
shahadat-osman <m.shahadat.osman@gmail.com>
2023-08-11 11:37:52 +0600

    solved +1

---
+++ Codeforces/100/080-Candies_Sisters.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int n, ans;
+    cin >> n;
+    ans = n - ((n / 2) + 1);
+    cout << ans << "\n";
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
